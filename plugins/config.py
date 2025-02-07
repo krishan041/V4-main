618ec278b9ec6fff0313248540e7a04e0536b8c8
krishan041 <krishandara04@gmail.com>
2025-02-07 22:10:42 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -41,11 +41,11 @@ class Config(object):
     
     SESSION_NAME = os.environ.get("SESSION_NAME", "UploadLinkToFileBot")
     
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002452189193"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002296948181"))
     
     LOGGER = logging
 
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002452189193")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002499165487")
     
     OWNER_ID = int(os.environ.get("OWNER_ID", "6428531614"))
     
